1ec0f928e2f37511e057dc16231cf0ecd4abc76d
brycx <brycx@protonmail.com>
2019-04-21 10:32:44 +0200

    Run constant-time comparison examples on equal length input.

--- example_non_ct/src/main.rs
+++ example_non_ct/src/main.rs
@@ -1,7 +1,12 @@
 
 fn main() {
-    fn not_constant_time_comparison(message: &[u8]) -> bool {
-        message == b"NOT CONSTANT TIME COMPARISON"
+    fn not_constant_time_comparison(m1: &[u8]) -> bool {
+        let mut m2 = m1.to_vec();
+        if !m2.is_empty() {
+            m2[0] ^= m1[0];
+        }
+
+        m1 == &m2[..]
     }
 
     let fuzzer = sidefuzz::SideFuzz::new(28, |message: &[u8]| {
--- subtle_ct_eq/src/main.rs
+++ subtle_ct_eq/src/main.rs
@@ -3,8 +3,13 @@ use subtle::ConstantTimeEq;
 fn main() {
     println!("This example target should be constant time, so it will never exit.");
 
-    let fuzzer = sidefuzz::SideFuzz::new(24, |message: &[u8]| {
-        sidefuzz::black_box(message.ct_eq(b"CONSTANT TIME COMPARISON"));
+    let fuzzer = sidefuzz::SideFuzz::new(24, |m1: &[u8]| {
+        let mut m2 = m1.to_vec();
+        if !m2.is_empty() {
+            m2[0] ^= m1[0];
+        }
+
+        sidefuzz::black_box(m2.ct_eq(m1));
         Ok(())
     });
 
